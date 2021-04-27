c70b27a76269982e344dde14a3695f28ce983486
André <abs@emancipe.se>
2021-04-27 01:52:20 -0300

    drop private dependency preventing webcrypto-core to be updated

--- app/capabilities.js
+++ app/capabilities.js
@@ -47,7 +47,7 @@ async function checkCrypto() {
   } catch (err) {
     try {
       window.asmCrypto = await import('asmcrypto.js');
-      await import('@dannycoates/webcrypto-liner/build/shim');
+      // HACK: remove some sort of support, needs investigation
       return true;
     } catch (e) {
       return false;
